dbb3091b133bd16e46b00fd5fae9d8fdba6fd2aa
Josh Sparkes <jshsparx@gmail.com>
2022-03-30 17:58:01 -0600

    add loadtweets function to fetch tweets using AJAX

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -10,31 +10,6 @@
 $(document).ready(function() {
 
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
   const renderTweets = function (tweets) {
     let returnValue = [];
     for (const tweet of tweets) {
@@ -73,7 +48,7 @@ $(document).ready(function() {
   // console.log("tweet", $tweet); // to see what it looks like
   // $('.tweet-container').append($tweet);
   
-  renderTweets(data);
+  // renderTweets(data);
 
 
   $('#tweet-form').on("submit", (function(event) {
@@ -84,11 +59,22 @@ $(document).ready(function() {
       data: $('#tweet-form').serialize(),
     })
     .then((data) => {
-      console.log(data);
+      // console.log(data);
     }) 
   }))
 
   
+  const loadTweets = function () {
+    $.ajax('/tweets', { 
+      method: 'GET',
+    })
+    .then((data) => {
+      console.log(data);
+      renderTweets(data);
+    })
+  }
+  loadTweets();
+  // console.log(loadedTweets);
   
 
   
